fc08165bbc4f0689263832690e796ce526c7743b
dominik <dominik.jacques@gmail.com>
2021-02-23 12:39:04 -0500

    version3 with output browser and more options

--- README.md
+++ README.md
@@ -1,44 +1,77 @@
-# Inkscape Export Layers 2
+# Inkscape Export Layers 3.0.0
 
 Inkscape extension to export SVG layers as standalone PDF, PNG, SVG or JPEG images.
-The exported images can be a combination of multiple layers.
+The exported images can progressively reveal content of an Inkscape document. 
+
+This version works with Inkscape version 1.
+
+The code here heavily relies on previous work by github users: @jespino and @dmitry-t. Great thanks to you both!
+Let me know if anyone else deserves credit for this. 
 
 ## How it works
 
-To export your Inkscape file as an image open an inkscape file with multiple layers
-(otherwise you can simply use the in-built export tool `Ctrl + E`).
-
 There are two options for your layers when exporting:
-- `[fixed]`: If a layer label starts with the prefix `[fixed]` this layer is always exported and
+- `[fixed] or [f]`: If a layer label starts with the prefix `[fixed]` or `[f]` this layer is always exported and
 combined with other layers. It is very useful for backgrounds or fixed elements.
-- `[export]`: If a layer label starts with `[export]` this layer is exported along with any `[fixed]` layer and
-combined into a single image.
+- `[export] or [e]`: If a layer label starts with `[export]` or `[e]` this layer is exported along with any `[fixed]` layer and
+combined into a single image. With the option `Progressively reveal elements of a figure`, exported layers are included one by one
+to the output images. 
 
 The order of layers remains untouched during export, i.e. a higher layer in the layer list is drawn above lower layers.
 
 **Example**
 
-![Layer export example](http://i.imgur.com/StUpSd1.png)
+![Base document](examples/source_screenshot_with_layers.png)
 
-The layer `[fixed] Background` is a fixed layer and will always be exported combined with the `[export]` layers.
+The layer `[f] always_show` is a fixed layer and will always be exported combined with the `[export]` layers.
 
-Once export is complete, we get two images: the combination of `version 1` + `Background` and
-`version 2` + `Background`.
+With the `Progressively reveal elements of a figure` checked, elements of the figures are progressively revealed.
+![Base document](examples/progressive_images.png)
+
+Without this option, elements of the figures are only composed of the fixed layer(s) and individual exported layers. 
+![Base document](examples/layer_by_layer.png)
+
 
 ## How to use
 
 To use the extension once your layers are ready:
 
+1. Get the code from the github page. (`Code -> Download ZIP` if you are unfamiliar with git clone)
+1. Put the `inkscape-export-layers` directory where Inkscape will find it. (`~/.config/inkscape/extensions/` will work for your user on linux)
+1. Open Inkscape
 1. Go to `Extensions` > `Export` > `Export layers...`
-1. Type or paste the path of a directory you want to export your files to (unfortunately,
-Inkscape does not support file selection dialogs for extensions).
-1. Choose area to export - either document canvas area or contents area. 
-1. Choose a format of your export - `PDF`, `PNG`, `SVG` or `JPEG` (`JPEG` required the `ImageMagick` installed in your system).
-1. Choose a required DPI.
-1. Choose whether to enumerate exported files with prefixes (`001_`, `002_` etc).
+1. Follow instructions in pop-up dialog and set options for your export
 1. Click `Apply`.
 
 ## Requirements
 
-1. Make sure the 'inkscape' program is available from the command line. This may not be the case if you are using flatpack installation.
+1. Make sure the 'Inkscape' program is available from the command line. This may not be the case if you are using flatpack installation.
 1. 'convert' must be available to export to jpeg
+
+## Use with beamer
+
+I mainly use this extension to make presentation with LateX+Beamer. Here are a few steps that will get you started in this direction.
+
+1. Pick a 16-9 aspect ratio for your source Inkscape document. 
+1. Setup the layers you want to export in the order that you would like. 
+1. Use the `Export layers` to output your figures
+1. Include the exported figures in the Beamer `.tex` file
+   ```
+   \documentclass[aspectratio=169,14pt]{beamer}
+   \usepackage{textpos}
+   \RequirePackage{graphicx}
+   \begin{document}
+   
+   %a block that progressively reveals content of Inkscape svg file
+   \begin{frame}
+       \begin{textblock*}{\paperwidth}(-1cm,-3.546cm)
+           \includegraphics<1>[width=\paperwidth]{layers/source_slide_01.pdf}
+           \includegraphics<2>[width=\paperwidth]{layers/source_slide_02.pdf}
+           \includegraphics<3>[width=\paperwidth]{layers/source_slide_03.pdf}
+       \end{textblock*}
+   \end{frame}
+   
+   \end{document}
+   ```
+1. Compile  (I highly recommend `latexmk -pdf` which will handle compilation on its own and recompile when source files change)
+
--- export_layers.py
+++ export_layers.py
@@ -15,8 +15,10 @@ import inkex
 Layer = collections.namedtuple('Layer', ['id', 'label', 'tag'])
 Export = collections.namedtuple('Export', ['visible_layers', 'file_name'])
 
-FIXED = '[fixed]'
+FIXED  = '[fixed]'
+F      = '[f]'
 EXPORT = '[export]'
+E      = '[e]'
 
 PDF = 'pdf'
 SVG = 'svg'
@@ -63,28 +65,33 @@ class LayerExport(inkex.Effect):
                                      dest='enumerate',
                                      default=None,
                                      help="suffix of files exported")
+        self.arg_parser.add_argument('--show-layers-below',
+                                     action='store',
+                                     type=str,
+                                     dest='show_layers_below',
+                                     default=None,
+                                     help="Show exported layers below the current layer")
 
     def effect(self):
         
         #process bool inputs that were read as strings
-        self.options.fit_contents = True if self.options.fit_contents == 'true' else False
-        self.options.enumerate    = True if self.options.enumerate    == 'true' else False
+        self.options.fit_contents      = True if self.options.fit_contents      == 'true' else False
+        self.options.enumerate         = True if self.options.enumerate         == 'true' else False
+        self.options.show_layers_below = True if self.options.show_layers_below == 'true' else False
 
         #get output dir from specified source file
         #otherwise set it as $HOME
         source = self.options.output_source
-        inkex.utils.debug(source)
         if os.path.isfile(source):
             output_dir = os.path.dirname(source)
-            prefix = os.path.splitext(os.path.basename(source))[0]
+            prefix = os.path.splitext(os.path.basename(source))[0]+'_'
         elif os.path.isdir(source):
             #change the default filled in by inkscape to $HOME
-            inkex.utils.debug(os.path.basename(source))
             if os.path.basename(source) == 'inkscape-export-layers':
                 output_dir = os.path.expanduser('~/')
                 prefix = ''
             else:
-                output_dir = source
+                output_dir = os.path.join(source)
                 prefix = ''
         else:
             raise Exception('output_source not a file or a dir...')
@@ -92,34 +99,36 @@ class LayerExport(inkex.Effect):
         #add subdir if one was passed
         output_dir = os.path.join(output_dir, self.options.output_subdir)
 
-        inkex.utils.debug(output_dir)
-        inkex.utils.debug(prefix)
-        exit()
-        if not os.path.exists(os.path.join(output_dir)):
-            os.makedirs(os.path.join(output_dir))
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
         layer_list = self.get_layer_list()
-        export_list = self.get_export_list(layer_list)
+        export_list = self.get_export_list(layer_list, self.options.show_layers_below)
         with _make_temp_directory() as tmp_dir:
             for export in export_list:
                 svg_file = self.export_to_svg(export, tmp_dir)
 
                 if self.options.file_type == PNG:
-                    if not self.convert_svg_to_png(svg_file, output_dir):
+                    if not self.convert_svg_to_png(svg_file, output_dir, prefix):
                         break
                 elif self.options.file_type == SVG:
-                    if not self.convert_svg_to_svg(svg_file, output_dir):
+                    if not self.convert_svg_to_svg(svg_file, output_dir, prefix):
                         break
                 elif self.options.file_type == PDF:
-                    if not self.convert_svg_to_pdf(svg_file, output_dir):
+                    if not self.convert_svg_to_pdf(svg_file, output_dir, prefix):
                         break
                 elif self.options.file_type == JPEG:
                     if not self.convert_png_to_jpeg(
-                            self.convert_svg_to_png(svg_file, tmp_dir),
-                            output_dir):
+                            self.convert_svg_to_png(svg_file, tmp_dir, prefix),
+                            output_dir, prefix):
                         break
 
     def get_layer_list(self):
+        """make a list od layers in source svg file
+            
+            Elements of the list are  of the form (id, label (layer name), tag ('[fixed]' or '[export]')
+                                                        
+        """
         svg_layers = self.document.xpath('//svg:g[@inkscape:groupmode="layer"]',
                                          namespaces=inkex.NSS)
         layer_list = []
@@ -135,34 +144,63 @@ class LayerExport(inkex.Effect):
             if layer_label.lower().startswith(FIXED):
                 layer_type = FIXED
                 layer_label = layer_label[len(FIXED):].lstrip()
+            elif layer_label.lower().startswith(F):
+                layer_type = FIXED
+                layer_label = layer_label[len(F):].lstrip()
             elif layer_label.lower().startswith(EXPORT):
                 layer_type = EXPORT
                 layer_label = layer_label[len(EXPORT):].lstrip()
+            elif layer_label.lower().startswith(E):
+                layer_type = EXPORT
+                layer_label = layer_label[len(E):].lstrip()
             else:
                 continue
 
             layer_list.append(Layer(layer_id, layer_label, layer_type))
 
-        # Layers are displayed in the reversed order in Inkscape compared to SVG
-        return list(reversed(layer_list))
+        return layer_list
 
-    def get_export_list(self, layer_list):
+    def get_export_list(self, layer_list, show_layers_below):
+        """selection of layers that should be visible
+
+            Each element of this list will be exported in its own file
+        """
         export_list = []
 
         for counter, layer in enumerate(layer_list):
+            #each layer marked as '[export]' is the basis for making a figure that will be exported
+
             if layer.tag == FIXED:
+                #Fixed layers are not the basis of exported figures
                 continue
+            elif layer.tag == EXPORT:
 
-            visible_layers = {
-                other_layer.id for other_layer in layer_list
-                if other_layer.tag == FIXED or other_layer.id == layer.id
-            }
+                #determine which other layers should appear in this figure
+                visible_layers = set()
+                layer_is_below = True
+                for other_layer in layer_list:
+                    if other_layer.tag == FIXED:
+                        #fixed layers appear in all figures
+                        #irrespective of their position relative to other layers
+                        visible_layers.add(other_layer.id)
+                    else:
+                        if other_layer.id == layer.id:
+                            #the basis layer for this figure is always visible
+                            visible_layers.add(other_layer.id)
+                            #all subsequent layers will be above
+                            layer_is_below = False
 
-            file_name = layer.label
-            if self.options.enumerate:
-                file_name = '%03d_%s' % (counter + 1, file_name)
+                        elif layer_is_below and show_layers_below:
+                            visible_layers.add(other_layer.id)
 
-            export_list.append(Export(visible_layers, file_name))
+                layer_name = layer.label
+                if self.options.enumerate:
+                    layer_name = '{:03d}_{}'.format(counter + 1, layer_name)
+
+                export_list.append(Export(visible_layers, layer_name))
+            else:
+                #layers not marked as FIXED of EXPORT are ignored
+                pass
 
         return export_list
 
@@ -189,15 +227,15 @@ class LayerExport(inkex.Effect):
 
         return output_file
 
-    def convert_svg_to_png(self, svg_file, output_dir):
+    def convert_svg_to_png(self, svg_file, output_dir, prefix):
         """
         Convert an SVG file into a PNG file.
         :param str svg_file: Path an input SVG file.
         :param str output_dir: Path to an output directory.
         :return Output file path.
         """
-        file_name = os.path.splitext(os.path.basename(svg_file))[0]
-        output_file = os.path.join(output_dir, file_name + '.png')
+        source_file_name = os.path.splitext(os.path.basename(svg_file))[0]
+        output_file = os.path.join(output_dir, prefix+source_file_name + '.png')
         command = [
             'inkscape',
             svg_file.encode('utf-8'),
@@ -214,15 +252,15 @@ class LayerExport(inkex.Effect):
 
         return output_file
 
-    def convert_svg_to_svg(self, svg_file, output_dir):
+    def convert_svg_to_svg(self, svg_file, output_dir, prefix):
         """
         Convert an [Inkscape] SVG file into a standard (plain) SVG file.
         :param str svg_file: Path an input SVG file.
         :param str output_dir: Path to an output directory.
         :return Output file path.
         """
-        file_name = os.path.splitext(os.path.basename(svg_file))[0]
-        output_file = os.path.join(output_dir, file_name + '.svg')
+        source_file_name = os.path.splitext(os.path.basename(svg_file))[0]
+        output_file = os.path.join(output_dir, prefix+source_file_name + '.svg')
         command = [
             'inkscape',
             svg_file.encode('utf-8'),    
@@ -240,15 +278,15 @@ class LayerExport(inkex.Effect):
 
         return output_file
 
-    def convert_svg_to_pdf(self, svg_file, output_dir):
+    def convert_svg_to_pdf(self, svg_file, output_dir, prefix):
         """
         Convert an [Inkscape] SVG file into a pdf.
         :param str svg_file: Path an input SVG file.
         :param str output_dir: Path to an output directory.
         :return Output file path.
         """
-        file_name = os.path.splitext(os.path.basename(svg_file))[0]
-        output_file = os.path.join(output_dir, file_name + '.pdf')
+        source_file_name = os.path.splitext(os.path.basename(svg_file))[0]
+        output_file = os.path.join(output_dir, prefix+source_file_name + '.pdf')
         command = [
             'inkscape',
             svg_file.encode('utf-8'),    
@@ -265,7 +303,7 @@ class LayerExport(inkex.Effect):
         return output_file
 
     @staticmethod
-    def convert_png_to_jpeg(png_file, output_dir):
+    def convert_png_to_jpeg(png_file, output_dir, prefix):
         """
         Convert a PNG file into a JPEG file.
         :param str png_file: Path an input PNG file.
@@ -275,8 +313,8 @@ class LayerExport(inkex.Effect):
         if png_file is None:
             return None
 
-        file_name = os.path.splitext(os.path.basename(png_file))[0]
-        output_file = os.path.join(output_dir, file_name + '.jpeg')
+        source_file_name = os.path.splitext(os.path.basename(png_file))[0]
+        output_file = os.path.join(output_dir, prefix+source_file_name + '.jpeg')
         command = ['convert', png_file, output_file]
         result = subprocess.run(command, capture_output=True)
         if result.returncode != 0:
